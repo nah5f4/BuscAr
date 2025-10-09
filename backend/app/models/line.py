61252e8d0902ef628234c868d5d135ddead402b3
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-10-09 11:06:08 -0300

    add Line model

---
+++ backend/app/models/line.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+from app.core.database import Base
+from sqlalchemy import Column
+from sqlalchemy import Enum as EnumDB
+from sqlalchemy import Integer, String
+
+
+class LineDirection(Enum):
+    MAIN = 1
+    SECONDARY = 2
+
+
+class Line(Base):
+    __tablename__ = "line"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    direction = Column(EnumDB(LineDirection))
