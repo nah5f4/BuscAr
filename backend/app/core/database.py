c61aa40be5a9ba56e89292cf22ef511cb0d7f366
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-10-11 22:30:05 -0300

    add missing `Base` declaration

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,9 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
@@ -9,4 +11,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+Base = declarative_base()
